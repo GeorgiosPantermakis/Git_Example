1e0be29f6a37fbce4fbb7a4bedc15ed9c0a2f211
GeorgiosPantermakis <gpanterm@gmail.com>
2020-09-13 18:10:36 +0300

    Filename option was added

--- main.py
+++ main.py
@@ -4,23 +4,27 @@ import sys
 import getopt
 from support_functions import read_data_json, store_data_json
 
-data = read_data_json(filename = 'wire_up.json')
-
-step_forword = None
+step_forward = None
 starting_point = None
+destination = None
 
 argv = sys.argv[1:]
 
-opts, args = getopt.getopt(argv, "s:f:",["starting_point=", "step_forword="])
+opts, args = getopt.getopt(argv, "s:f:d:",["starting_point=", "step_forward=","destination="])
 
 for opt, arg in opts:
     if opt in ('-s', '--starting_point'):
         starting_point = int(arg)
-    elif opt in ('-f', '--step_forword'):
-        step_forword = int(arg)
-print(starting_point)
-step_forword = 1 if step_forword is None else step_forword
+    elif opt in ('-f', '--step_forward'):
+        step_forward = int(arg)
+    elif opt in ('-d', '--destination'):
+        destination = arg    
+
+step_forward = 1 if step_forward is None else step_forward
 starting_point = 1 if starting_point is None else starting_point
+destination = 'wire_up.json' if destination is None else destination
+
+data = read_data_json(filename = destination)
 
 characters = string.ascii_lowercase + string.ascii_uppercase + ' '
 new_data = {}
@@ -37,7 +41,7 @@ for key, value in data.items():
             break
     old_number_of_placeholder = int(old_number_of_placeholder)
     if old_number_of_placeholder >= starting_point:
-        new_number_of_placeholder = old_number_of_placeholder + step_forword
+        new_number_of_placeholder = old_number_of_placeholder + step_forward
         placeholder = str(new_number_of_placeholder) + " " + key[len(str(new_number_of_placeholder))+1:]
         new_data[placeholder] = value
     else:
