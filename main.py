0a56b91fdd1968cc4b66afaae5514c5b1934329a
GeorgiosPantermakis <gpanterm@gmail.com>
2020-09-19 22:39:46 +0300

    Applied the black module in my scripts

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import os
-import sys 
+import sys
 import getopt
-from support_functions import read_data_json
-from support_functions import store_data_json
-from support_functions import number_of_placeholder
-from support_functions import frequency_table
+from support_functions import number_of_placeholder, frequency_table, read_data_json, store_data_json
 
 step_forward = None
 starting_point = None
@@ -12,56 +9,43 @@ path = None
 
 argv = sys.argv[1:]
 
-opts, args = getopt.getopt(argv, "s:f:d:",["starting_point=", "step_forward=","path="])
+opts, args = getopt.getopt(argv, "s:f:d:", ["starting_point=", "step_forward=", "path="])
 
 for opt, arg in opts:
-    if opt in ('-s', '--starting_point'):
+    if opt in ("-s", "--starting_point"):
         starting_point = int(arg)
-    elif opt in ('-f', '--step_forward'):
+    elif opt in ("-f", "--step_forward"):
         step_forward = int(arg)
-    elif opt in ('-p', '--path'):
+    elif opt in ("-p", "--path"):
         path = arg
 
 step_forward = 1 if step_forward is None else step_forward
 starting_point = 1 if starting_point is None else starting_point
-path = 'wire_up.json' if path is None else path
+path = "wire_up.json" if path is None else path
 
-data = read_data_json(filename = path)
+data = read_data_json(filename=path)
 
 new_data = {}
 freq = frequency_table(data)
 counter = 0
 for key, value in data.items():
     counter += 1
-    current_number_of_placeholder = number_of_placeholder(placeholder_info = key)
+    current_number_of_placeholder = number_of_placeholder(placeholder_info=key)
     if current_number_of_placeholder >= starting_point:
         new_number_of_placeholder = current_number_of_placeholder + step_forward
-        placeholder = str(new_number_of_placeholder) + " " + key[len(str(current_number_of_placeholder))+1:]
+        placeholder = (str(new_number_of_placeholder) + " " + key[len(str(current_number_of_placeholder)) + 1 :])
         new_data[placeholder] = value
-        fake_placeholder = new_number_of_placeholder 
+        fake_placeholder = new_number_of_placeholder
     else:
-        placeholder = str(current_number_of_placeholder) + " " + key[len(str(current_number_of_placeholder))+1:]
+        placeholder = (str(current_number_of_placeholder) + " " + key[len(str(current_number_of_placeholder)) + 1 :])
         new_data[placeholder] = value
-        fake_placeholder = current_number_of_placeholder 
+        fake_placeholder = current_number_of_placeholder
 
     if freq[current_number_of_placeholder] == counter:
-        fake_placeholder = str(fake_placeholder)  + " Drop this textline"
+        fake_placeholder = str(fake_placeholder) + " Drop this textline"
         new_data[fake_placeholder] = ""
         counter = 0
 
-new_wire_up = store_data_json(content = new_data)
+new_wire_up = store_data_json(content=new_data)
 filepath = os.getcwd() + "\\" + new_wire_up
-print("The new wire-up.json stored in {path}".format(path = filepath))
-
-
-
-
-# bounder = starting_point
-# copy_dict = new_data.copy()
-# for key, value in copy_dict.items():
-#     current_number_of_placeholder = number_of_placeholder(placeholder_info = key)
-#     print(bounder, current_number_of_placeholder, bounder < current_number_of_placeholder)
-#     if bounder < current_number_of_placeholder:
-#         bounder = current_number_of_placeholder
-#         placeholder = str(current_number_of_placeholder) + " " + key[len(str(current_number_of_placeholder))+1:]
-#         new_data[placeholder + "X"] = ""
+print("The new wire-up.json stored in {path}".format(path=filepath))
--- support_functions.py
+++ support_functions.py
@@ -1,8 +1,9 @@
 import json
 import string
 
+
 def number_of_placeholder(placeholder_info):
-    characters = string.ascii_lowercase + string.ascii_uppercase + ' '
+    characters = string.ascii_lowercase + string.ascii_uppercase + " "
     start = 0
     end = 1
     num_of_placeholder = ""
@@ -10,10 +11,11 @@ def number_of_placeholder(placeholder_info):
         if placeholder_info[start:end] not in characters:
             num_of_placeholder += placeholder_info[start:end]
             start += 1
-            end += 1 
+            end += 1
         else:
             break
     return int(num_of_placeholder)
+
 
 def frequency_table(dict):
     freq_dict = {}
@@ -21,24 +23,26 @@ def frequency_table(dict):
         key_freq_dict = number_of_placeholder(key)
         if key_freq_dict not in freq_dict.keys():
             freq_dict[key_freq_dict] = 1
-        else:    
+        else:
             freq_dict[key_freq_dict] += 1
-    return freq_dict      
+    return freq_dict
+
 
 def read_data_json(filename):
-    
-    # Opening JSON file 
+
+    # Opening JSON file
     with open(filename) as read_file:
 
         # returns JSON object as a dictionary
         data = json.load(read_file)
-    return data    
+    return data
+
 
 def store_data_json(content):
 
     # The json file where the output must be stored
-    with open('wire_up_new.json', 'w') as out_file:
-    
+    with open("wire_up_new.json", "w") as out_file:
+
         # Returns JSON object as a dictionary
-        json.dump(content, out_file, indent = 6)
-    return 'wire_up_new.json'
+        json.dump(content, out_file, indent=6)
+    return "wire_up_new.json"
