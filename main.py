edbfabcde8a0ce0b37eb901993c09da013c27456
GeorgiosPantermakis <gpanterm@gmail.com>
2020-09-20 02:12:12 +0300

    Addition of new command line

--- main.py
+++ main.py
@@ -3,25 +3,24 @@ import sys
 import getopt
 from support_functions import number_of_placeholder, frequency_table, read_data_json, store_data_json
 
-step_forward = None
-starting_point = None
-path = None
+step = 0
+beginning = 1
+path = "wire_up.json"
+textline = False
 
 argv = sys.argv[1:]
 
-opts, args = getopt.getopt(argv, "s:f:d:", ["starting_point=", "step_forward=", "path="])
+opts, args = getopt.getopt(argv, "s:f:b:t:", ["beginning=", "step=", "path=", "textline="])
 
 for opt, arg in opts:
-    if opt in ("-s", "--starting_point"):
-        starting_point = int(arg)
-    elif opt in ("-f", "--step_forward"):
-        step_forward = int(arg)
+    if opt in ("-b", "--beginning"):
+        beginning = int(arg)
+    elif opt in ("-s", "--step"):
+        step = int(arg)
     elif opt in ("-p", "--path"):
         path = arg
-
-step_forward = 1 if step_forward is None else step_forward
-starting_point = 1 if starting_point is None else starting_point
-path = "wire_up.json" if path is None else path
+    elif opt in ("-t", "--textline"):
+        textline = arg
 
 data = read_data_json(filename=path)
 
@@ -31,8 +30,8 @@ counter = 0
 for key, value in data.items():
     counter += 1
     current_number_of_placeholder = number_of_placeholder(placeholder_info=key)
-    if current_number_of_placeholder >= starting_point:
-        new_number_of_placeholder = current_number_of_placeholder + step_forward
+    if current_number_of_placeholder >= beginning:
+        new_number_of_placeholder = current_number_of_placeholder + step
         placeholder = (str(new_number_of_placeholder) + " " + key[len(str(current_number_of_placeholder)) + 1 :])
         new_data[placeholder] = value
         fake_placeholder = new_number_of_placeholder
@@ -41,7 +40,7 @@ for key, value in data.items():
         new_data[placeholder] = value
         fake_placeholder = current_number_of_placeholder
 
-    if freq[current_number_of_placeholder] == counter:
+    if freq[current_number_of_placeholder] == counter and textline:
         fake_placeholder = str(fake_placeholder) + " Drop this textline"
         new_data[fake_placeholder] = ""
         counter = 0
